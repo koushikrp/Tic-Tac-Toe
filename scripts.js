0fbac18aec346a65e3cf9aa2a4833e23b4c76831
Koushik <koushikrp26@gmail.com>
2021-06-17 21:50:07 +0530

    AI plays perfectly and maximum a player can do is a draw- Used MiniMax algorithm to optimally select a block each time the player plays

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Board module
 gameBoard = (function (doc) {
 	let board = ["", "", "", "", "", "", "", "", ""];
 	getBoard = () => {
@@ -10,13 +11,97 @@ gameBoard = (function (doc) {
 	changeBoardItem = (index, value) => {
 		board[index] = value;
 	};
+	allFilled = () => {
+		for (let i = 0; i < 9; i++) if (board[i] === "") return false;
+		return true;
+	};
+	evaluate = () => {
+		for (let i = 0; i < 9; i += 3) {
+			if (board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
+				if (board[i] === "O") return 10;
+				if (board[i] === "X") return -10;
+			}
+		}
+		for (let i = 0; i < 3; i++) {
+			if (board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
+				if (board[i] === "O") return 10;
+				if (board[i] === "X") return -10;
+			}
+		}
+		if ((board[0] === board[4] && board[4] === board[8]) || (board[2] === board[4] && board[4] === board[6])) {
+			if (board[4] === "O") return 10;
+			if (board[4] === "X") return -10;
+		}
+		return 0;
+	};
+	miniMax = (isMaximum) => {
+		let stateStatus = evaluate();
+		if (stateStatus === 10 || stateStatus === -10) return stateStatus;
+		if (allFilled()) return 0;
+		let bestValue;
+		if (isMaximum) {
+			bestValue = -1e4;
+			for (let i = 0; i < 9; i++) {
+				if (board[i] === "") {
+					board[i] = "O";
+					bestValue = Math.max(bestValue, miniMax(false));
+					board[i] = "";
+				}
+			}
+		} else {
+			bestValue = 1e4;
+			for (let i = 0; i < 9; i++) {
+				if (board[i] === "") {
+					board[i] = "X";
+					bestValue = Math.min(bestValue, miniMax(true));
+					board[i] = "";
+				}
+			}
+		}
+		return bestValue;
+	};
+	findBestMove = () => {
+		let bestPoints = -1e4;
+		let bestMove, curPoints;
+		for (let i = 0; i < 9; i++) {
+			if (board[i] === "") {
+				board[i] = "O";
+				curPoints = miniMax(false);
+				board[i] = "";
+				if (curPoints > bestPoints) {
+					bestPoints = curPoints;
+					bestMove = i;
+				}
+			}
+		}
+		return bestMove;
+	};
 	return {
 		getBoard,
 		resetBoard,
 		changeBoardItem,
+		allFilled,
+		findBestMove,
+		evaluate,
 	};
 })(document);
 
+//Display module
+displayController = (function (doc) {
+	render = () => {
+		let curBoard = gameBoard.getBoard();
+		let itr = 0;
+		curBoard.forEach((element) => {
+			doc.querySelector(`.pos${itr + 1}`).textContent = element;
+			itr++;
+		});
+	};
+	return {
+		render,
+	};
+})(document);
+
+//Player factory function
 player = () => {
 	const setName = (name) => {
 		this.name = name;
@@ -30,113 +115,70 @@ player = () => {
 	const getSymbol = () => {
 		return this.symbol;
 	};
+	play = () => {
+		document.querySelector(".submitName").addEventListener("click", (e) => {
+			let enteredName = document.querySelector(".name").value;
+			document.querySelector(".name").value = "";
+			player1.setName(enteredName);
+			document.getElementById("playerturn").textContent = enteredName + " Turn";
+		});
+
+		document.querySelector(".start").addEventListener("click", () => {
+			document.querySelector(".results").textContent = "";
+			gameBoard.resetBoard();
+			displayController.render();
+		});
+
+		markers = document.getElementsByClassName("element");
+		[...markers].forEach((marker) => {
+			marker.addEventListener("click", (e) => {
+				const announceResults = document.querySelector(".results");
+				document.querySelector(".results").textContent = "";
+				if (e.target.textContent !== "") return;
+				gameBoard.changeBoardItem(e.target.classList[1][3] - 1, player1.getSymbol());
+				displayController.render();
+
+				if (gameBoard.evaluate() === 10 || gameBoard.evaluate() === -10) {
+					announceResults.innerText = "GAME OVER: " + `${player1.getName()}` + " WON!";
+					gameBoard.resetBoard();
+					render();
+				} else if (gameBoard.allFilled()) {
+					announceResults.innerText = "GAME OVER: " + "It's a DRAW.";
+					gameBoard.resetBoard();
+					render();
+				} else {
+					document.getElementById("playerturn").style.backgroundColor = "transparent";
+					document.getElementById("aiturn").style.backgroundColor = "rgb(163, 66, 87)";
+					// while (gameBoard.getBoard()[(r = ~~(Math.random() * 9))] !== "");
+					let r = gameBoard.findBestMove();
+					gameBoard.changeBoardItem(r, "O");
+					displayController.render();
+					if (gameBoard.evaluate() === 10 || gameBoard.evaluate() === -10) {
+						announceResults.innerText = "GAME OVER: AI WON!";
+						gameBoard.resetBoard();
+						render();
+					} else if (gameBoard.allFilled()) {
+						announceResults.innerText = "GAME OVER: " + "It's a DRAW.";
+						gameBoard.resetBoard();
+						render();
+					}
+					setTimeout(() => {
+						document.getElementById("playerturn").style.backgroundColor = "rgb(163, 66, 87)";
+						document.getElementById("aiturn").style.backgroundColor = "transparent";
+					}, 100);
+				}
+			});
+		});
+	};
 	return {
 		setName,
 		getName,
 		setSymbol,
 		getSymbol,
+		play,
 	};
 };
 
-displayController = (function (doc) {
-	render = () => {
-		let curBoard = gameBoard.getBoard();
-		let itr = 0;
-		curBoard.forEach((element) => {
-			doc.querySelector(`.pos${itr + 1}`).textContent = element;
-			itr++;
-		});
-	};
-	isGameOver = (boardStatus, curPlayerSymbol, curPlayerName) => {
-		const announceResults = doc.querySelector(".results");
-		if (
-			(boardStatus[0] === curPlayerSymbol &&
-				boardStatus[1] === curPlayerSymbol &&
-				boardStatus[2] === curPlayerSymbol) ||
-			(boardStatus[0] === curPlayerSymbol &&
-				boardStatus[4] === curPlayerSymbol &&
-				boardStatus[8] === curPlayerSymbol) ||
-			(boardStatus[0] === curPlayerSymbol &&
-				boardStatus[3] === curPlayerSymbol &&
-				boardStatus[6] === curPlayerSymbol) ||
-			(boardStatus[1] === curPlayerSymbol &&
-				boardStatus[4] === curPlayerSymbol &&
-				boardStatus[7] === curPlayerSymbol) ||
-			(boardStatus[2] === curPlayerSymbol &&
-				boardStatus[5] === curPlayerSymbol &&
-				boardStatus[8] === curPlayerSymbol) ||
-			(boardStatus[3] === curPlayerSymbol &&
-				boardStatus[4] === curPlayerSymbol &&
-				boardStatus[5] === curPlayerSymbol) ||
-			(boardStatus[6] === curPlayerSymbol &&
-				boardStatus[7] === curPlayerSymbol &&
-				boardStatus[8] === curPlayerSymbol) ||
-			(boardStatus[2] === curPlayerSymbol &&
-				boardStatus[4] === curPlayerSymbol &&
-				boardStatus[6] === curPlayerSymbol)
-		) {
-			announceResults.innerText = "GAME OVER: " + `${curPlayerName}` + " WON!";
-			gameBoard.resetBoard();
-			render();
-			return true;
-		}
-		if (
-			boardStatus[0] !== "" &&
-			boardStatus[1] !== "" &&
-			boardStatus[2] !== "" &&
-			boardStatus[3] !== "" &&
-			boardStatus[4] !== "" &&
-			boardStatus[5] !== "" &&
-			boardStatus[6] !== "" &&
-			boardStatus[7] !== "" &&
-			boardStatus[8] !== ""
-		) {
-			announceResults.innerText = "GAME OVER: " + "It's a DRAW.";
-			gameBoard.resetBoard();
-			render();
-			return true;
-		}
-		return false;
-	};
-	doc.querySelector(".start").addEventListener("click", () => {
-		doc.querySelector(".results").textContent = "";
-		gameBoard.resetBoard();
-		render();
-	});
-	markers = doc.getElementsByClassName("element");
-	[...markers].forEach((marker) => {
-		marker.addEventListener("click", (e) => {
-			doc.querySelector(".results").textContent = "";
-			if (e.target.textContent !== "") return;
-			gameBoard.changeBoardItem(e.target.classList[1][3] - 1, player1.getSymbol());
-			render();
-			let currentBoard = gameBoard.getBoard();
-			if (!isGameOver(currentBoard, player1.getSymbol(), player1.getName())) {
-				let r;
-				doc.getElementById("playerturn").style.backgroundColor = "transparent";
-				doc.getElementById("aiturn").style.backgroundColor = "rgb(163, 66, 87)";
-				while (currentBoard[(r = ~~(Math.random() * 9))] !== "");
-				gameBoard.changeBoardItem(r, "O");
-				render();
-				isGameOver(gameBoard.getBoard(), "O", "AI");
-				setTimeout(() => {
-					doc.getElementById("playerturn").style.backgroundColor = "rgb(163, 66, 87)";
-					doc.getElementById("aiturn").style.backgroundColor = "transparent";
-				}, 100);
-			}
-		});
-	});
-	return {
-		render,
-	};
-})(document);
-
-displayController.render();
 player1 = player();
 player1.setSymbol("X");
-document.querySelector(".submitName").addEventListener("click", (e) => {
-	let enteredName = document.querySelector(".name").value;
-	document.querySelector(".name").value = "";
-	player1.setName(enteredName);
-	document.getElementById("playerturn").textContent = enteredName + " Turn";
-});
+player1.play();
